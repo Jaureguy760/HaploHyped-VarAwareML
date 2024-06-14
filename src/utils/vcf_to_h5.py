2e9de3516c455f1a2109282d451e1d2e3106c7d5
Jeff Jaureguy <67065808+Jaureguy760@users.noreply.github.com>
2024-06-14 16:54:49 -0700

    Update vcf_to_h5.py

--- src/utils/vcf_to_h5.py
+++ src/utils/vcf_to_h5.py
@@ -1,8 +1,32 @@
 import os
+import argparse
+import h5py
 import polars as pl
 import numpy as np
-import h5py
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
+import gzip
+import shutil
+
+def read_sample_list(sample_list_path):
+    with open(sample_list_path, 'r') as f:
+        return [line.strip() for line in f]
+
+def store_individuals(data_path, save_path, study_name):
+    """
+    Store individual sample names into an HDF5 file with compression.
+    
+    Parameters:
+    data_path (str): Path to the sample list file.
+    save_path (str): Path to save the HDF5 file.
+    study_name (str): Name of the study.
+    """
+    h5_file = os.path.join(save_path, f'{study_name}.h5')
+    individuals = read_sample_list(data_path)
+    
+    with h5py.File(h5_file, 'a') as h5_gen_file:
+        if 'individuals' in h5_gen_file:
+            del h5_gen_file['individuals']
+        h5_gen_file.create_dataset('individuals', data=np.array(individuals, dtype='S'), compression='gzip', compression_opts=5)
 
 def nucleotide_to_index(seq):
     """
@@ -30,102 +54,44 @@ def bitpack_indices(indices):
     packed = np.packbits(indices.reshape(-1, 4), axis=-1, bitorder='little')
     return packed
 
-def probes_VCF2hdf5(data_path, save_path, study_name, chunk_size=100000):
-    """
-    Process VCF file and store probe data into an HDF5 file with compression.
-    
-    Parameters:
-    data_path (str): Path to the input VCF file.
-    save_path (str): Path to save the HDF5 file.
-    study_name (str): Name of the study.
-    chunk_size (int): Number of rows to process at a time.
-    """
-    h5_file = os.path.join(save_path, 'probes', study_name + '.h5')
-    
-    # Check if the HDF5 file exists and is up-to-date
-    if os.path.isfile(h5_file) and os.path.getmtime(data_path) <= os.path.getmtime(h5_file):
-        print(f"{h5_file} is already up-to-date.")
-        return
-
-    if os.path.isfile(h5_file):
-        os.remove(h5_file)
-
-    hash_table = {'keys': np.array([], dtype=np.int64), 'allele': np.array([], dtype='S')}
-    for chunk in pl.read_csv(data_path, sep='\t', chunksize=chunk_size, has_header=False):
-        with h5py.File(h5_file, 'w') as hf:
-            chunk = chunk.to_pandas()
-            chunk.columns = ["CHR", "bp", "ID", 'allele1', 'allele2', 'QUAL', 'FILTER', 'INFO']
-            allele1_indices = chunk['allele1'].map({'A': 0, 'C': 1, 'G': 2, 'T': 3}).astype(np.int8)
-            allele2_indices = chunk['allele2'].map({'A': 0, 'C': 1, 'G': 2, 'T': 3}).astype(np.int8)
-            
-            # Bitpack the indices
-            packed_allele1 = bitpack_indices(allele1_indices)
-            packed_allele2 = bitpack_indices(allele2_indices)
-            
-            packed_chunk = np.column_stack((chunk[["CHR", "bp", "ID"]], packed_allele1, packed_allele2))
-
-            if 'data' not in hf:
-                maxshape = (None, packed_chunk.shape[1])
-                # Using default chunking provided by HDF5
-                dset = hf.create_dataset('data', data=packed_chunk, maxshape=maxshape, compression='lzf')
-            else:
-                hf['data'].resize((hf['data'].shape[0] + packed_chunk.shape[0]), axis=0)
-                hf['data'][-packed_chunk.shape[0]:] = packed_chunk
-
-    pl.DataFrame(hash_table).write_csv(os.path.join(save_path, 'probes', study_name + '_hash_table.csv.gz'), compression='gzip')
-
-def ind_VCF2hdf5(data_path, save_path, study_name):
-    """
-    Process VCF file and store individual data into an HDF5 file with compression.
-    
-    Parameters:
-    data_path (str): Path to the input VCF file.
-    save_path (str): Path to save the HDF5 file.
-    study_name (str): Name of the study.
-    """
-    h5_file = os.path.join(save_path, 'individuals', study_name + '.h5')
-    
-    # Check if the HDF5 file exists and is up-to-date
-    if os.path.isfile(h5_file) and os.path.getmtime(data_path) <= os.path.getmtime(h5_file):
-        print(f"{h5_file} is already up-to-date.")
-        return
-    
-    if os.path.isfile(h5_file):
-        os.remove(h5_file)
-
-    individuals = pl.read_csv(data_path, has_header=False).to_series(0).to_list()
-    
-    with h5py.File(h5_file, 'w') as hf:
-        # Using default chunking provided by HDF5
-        hf.create_dataset('individuals', data=np.array(individuals, dtype='S'), compression='lzf')
-
-def genotype_VCF2hdf5(data_path, id, save_path, study_name):
+def genotype_VCF2hdf5(data_path, donor_id, chromosome, save_path, study_name):
     """
     Process VCF file and store genotype data into an HDF5 file with compression.
     
     Parameters:
     data_path (str): Path to the input VCF file.
-    id (int): Identifier for the genotype.
+    donor_id (int): Identifier for the donor.
+    chromosome (int): Chromosome number.
     save_path (str): Path to save the HDF5 file.
     study_name (str): Name of the study.
     """
-    h5_file = os.path.join(save_path, 'genotype', str(id) + '_' + study_name + '.h5')
-    
-    # Check if the HDF5 file exists and is up-to-date
-    if os.path.isfile(h5_file) and os.path.getmtime(data_path) <= os.path.getmtime(h5_file):
-        print(f"{h5_file} is already up-to-date.")
-        return
+    tmp_h5_file = os.path.join(save_path, f'{study_name}_tmp_{donor_id}_{chromosome}.h5')
 
-    df = pl.read_csv(data_path, has_header=False).to_pandas()
-    data = df.to_numpy()
-    
-    with h5py.File(h5_file, 'w') as hf:
-        # Using default chunking provided by HDF5
-        hf.create_dataset('genotype', data=data.astype(np.int8), compression='lzf')
+    columns = []
+    with gzip.open(data_path, 'rt') as file:
+        for line in file:
+            if line.startswith('#CHROM'):
+                columns = line.strip().split('\t')
+                break
 
-    os.remove(data_path)
+    df = pl.read_csv(data_path, sep='\t', comment='#', compression='gzip', has_header=False)
+    df.columns = columns
+    genotype_cols = columns[9:]
 
-def process_chromosome(chromosome, vcf_dir, out_dir, study_name):
+    df_genotypes = df.select(genotype_cols).apply(lambda x: x if isinstance(x, str) and '|' in x else None)
+    df_positions = df.select(['#CHROM', 'POS', 'ID', 'REF', 'ALT'])
+
+    merged_df = pl.concat([df_positions, df_genotypes], how='horizontal')
+
+    with h5py.File(tmp_h5_file, 'a') as h5_gen_file:
+        group_path = f'donor_{donor_id}/chr_{chromosome}'
+        group = h5_gen_file.require_group(group_path)
+        if 'genotype' in group:
+            del group['genotype']
+        packed_genotypes = bitpack_indices(merged_df.select(genotype_cols).to_numpy().astype(np.int8))
+        group.create_dataset('genotype', data=packed_genotypes, compression='lzf', chunks=True)
+
+def process_chromosome(chromosome, vcf_dir, out_dir, study_name, donor_ids):
     """
     Process a single chromosome's VCF file and store the data into HDF5 files.
     
@@ -134,22 +100,56 @@ def process_chromosome(chromosome, vcf_dir, out_dir, study_name):
     vcf_dir (str): Directory containing VCF files.
     out_dir (str): Directory to save the output HDF5 files.
     study_name (str): Name of the study.
+    donor_ids (list): List of donor IDs to process.
     """
     vcf_file = os.path.join(vcf_dir, f'chr{chromosome}.filtered.vcf.gz')
-    os.makedirs(out_dir, exist_ok=True)
-    os.makedirs(os.path.join(out_dir, 'genotype'), exist_ok=True)
-    os.makedirs(os.path.join(out_dir, 'individuals'), exist_ok=True)
-    os.makedirs(os.path.join(out_dir, 'probes'), exist_ok=True)
-    os.makedirs(os.path.join(out_dir, 'tmp_files'), exist_ok=True)
+    for donor_id in donor_ids:
+        genotype_VCF2hdf5(vcf_file, donor_id, chromosome, out_dir, study_name)
+
+def merge_h5_files(tmp_dir, final_h5_file):
+    """
+    Merge temporary HDF5 files into a single final HDF5 file.
     
-    probes_VCF2hdf5(vcf_file, out_dir, study_name, chunk_size=100000)  # Adjust chunk size here
-    ind_VCF2hdf5(vcf_file, out_dir, study_name)
+    Parameters:
+    tmp_dir (str): Directory containing temporary HDF5 files.
+    final_h5_file (str): Path to the final HDF5 file.
+    """
+    with h5py.File(final_h5_file, 'a') as final_file:
+        for tmp_file in os.listdir(tmp_dir):
+            if tmp_file.endswith(".h5"):
+                tmp_file_path = os.path.join(tmp_dir, tmp_file)
+                with h5py.File(tmp_file_path, 'r') as tmp:
+                    for donor in tmp.keys():
+                        if donor not in final_file:
+                            final_file.create_group(donor)
+                        for chrom in tmp[donor].keys():
+                            group_path = f"{donor}/{chrom}"
+                            if group_path in final_file:
+                                del final_file[group_path]
+                            tmp.copy(f"{donor}/{chrom}", final_file)
 
 if __name__ == "__main__":
-    vcf_dir = "/path/to/vcf_dir"
-    out_dir = "/path/to/output_dir"
-    study_name = "your_study_name"
-    chromosomes = range(1, 23)
+    parser = argparse.ArgumentParser(description='Script to convert VCF data')
+    parser.add_argument("--cohort_name", required=True, type=str, help="Cohort specific name")
+    parser.add_argument("--vcf", required=True, type=str, help="Path to VCF files directory")
+    parser.add_argument("--outdir", required=True, type=str, help="Path to results save folder")
+    parser.add_argument("--flag", required=True, type=str, choices=['individuals', 'chunk'], help="Type of data to process")
+    parser.add_argument("--sample_list", required=False, type=str, help="Path to sample list file", default='sample_list.txt')
 
-    with Pool(processes=4) as pool:
-        pool.starmap(process_chromosome, [(chr, vcf_dir, out_dir, study_name) for chr in chromosomes])
+    args = parser.parse_args()
+
+    tmp_dir = os.path.join(args.outdir, 'tmp_files')
+    os.makedirs(tmp_dir, exist_ok=True)
+    
+    if args.flag == 'individuals':
+        store_individuals(args.sample_list, args.outdir, args.cohort_name)
+    elif args.flag == 'chunk':
+        donor_ids = read_sample_list(args.sample_list)
+        chromosomes = range(1, 23)
+        num_processes = cpu_count() - 4
+        with Pool(processes=num_processes) as pool:
+            pool.starmap(process_chromosome, [(chr, args.vcf, tmp_dir, args.cohort_name, donor_ids) for chr in chromosomes])
+
+        final_h5_file = os.path.join(args.outdir, f'{args.cohort_name}.h5')
+        merge_h5_files(tmp_dir, final_h5_file)
+        shutil.rmtree(tmp_dir)
