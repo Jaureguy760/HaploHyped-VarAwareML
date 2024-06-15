0e302c6b71aebe13127f0c5641071c5bf740fbd9
Jeff Jaureguy <67065808+Jaureguy760@users.noreply.github.com>
2024-06-14 17:34:48 -0700

    Update h5_reader.py

--- src/utils/h5_reader.py
+++ src/utils/h5_reader.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+from .common_utils import unpack_bitpacked_data, nucleotide_to_index, parse_encode_dict
 
 class HDF5Reader:
     def __init__(self, h5_file):
@@ -9,7 +10,8 @@ class HDF5Reader:
         with h5py.File(self.h5_file, 'r') as f:
             group_path = f'donor_{donor_id}/chr_{chromosome}'
             if group_path in f:
-                genotype_data = f[group_path]['genotype'][()]
-                return genotype_data
+                packed_data = f[group_path]['genotype'][()]
+                unpacked_data = unpack_bitpacked_data(packed_data)
+                return unpacked_data
             else:
                 raise KeyError(f"No data found for {group_path}")
