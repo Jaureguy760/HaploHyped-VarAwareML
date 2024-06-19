f80f2defff907409b1869cddd90f0f60db243f33
Jeff Jaureguy <67065808+Jaureguy760@users.noreply.github.com>
2024-06-19 16:26:20 -0700

    Update vcf_to_h5.py

--- src/utils/vcf_to_h5.py
+++ src/utils/vcf_to_h5.py
@@ -22,6 +22,15 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def read_sample_list(sample_list_path):
+    """
+    Read the sample list file and return a list of sample names.
+
+    Parameters:
+    sample_list_path (str): Path to the sample list file.
+
+    Returns:
+    list: A list of sample names.
+    """
     try:
         with open(sample_list_path, 'r') as f:
             return [line.strip() for line in f]
@@ -30,6 +39,14 @@ def read_sample_list(sample_list_path):
         raise
 
 def store_individuals(data_path, save_path, study_name):
+    """
+    Store individual sample names into an HDF5 file with compression.
+
+    Parameters:
+    data_path (str): Path to the sample list file.
+    save_path (str): Path to save the HDF5 file.
+    study_name (str): Name of the study.
+    """
     logger.info(f"Storing individuals from {data_path} to {save_path}")
     h5_file = os.path.join(save_path, f'{study_name}.h5')
     individuals = read_sample_list(data_path)
@@ -41,9 +58,20 @@ def store_individuals(data_path, save_path, study_name):
     logger.info("Finished storing individuals")
 
 def genotype_VCF2hdf5(data_path, donor_id, chromosome, save_path, study_name):
+    """
+    Process VCF file and store genotype data into an HDF5 file with compression.
+
+    Parameters:
+    data_path (str): Path to the input VCF file.
+    donor_id (str): Identifier for the donor.
+    chromosome (int): Chromosome number.
+    save_path (str): Path to save the HDF5 file.
+    study_name (str): Name of the study.
+    """
     logger.info(f"Processing VCF file {data_path} for donor {donor_id} and chromosome {chromosome}")
     tmp_h5_file = os.path.join(save_path, f'{study_name}_tmp_{donor_id}_{chromosome}.h5')
 
+    # Read column names from VCF header
     columns = []
     with gzip.open(data_path, 'rt') as file:
         for line in file:
@@ -51,19 +79,23 @@ def genotype_VCF2hdf5(data_path, donor_id, chromosome, save_path, study_name):
                 columns = line.strip().split('\t')
                 break
 
+    # Read VCF data into DataFrame
     df = pl.read_csv(data_path, sep='\t', comment='#', compression='gzip', has_header=False)
     df.columns = columns
     genotype_cols = columns[9:]
 
+    # Filter and transform genotype data
     df_genotypes = df.select(genotype_cols).apply(lambda x: x if isinstance(x, str) and '|' in x else None)
     df_positions = df.select(['#CHROM', 'POS', 'ID', 'REF', 'ALT'])
     
+    # Convert nucleotides to indices
     df_positions = df_positions.with_columns([
         pl.col('REF').apply(nucleotide_to_index).alias('REF_IDX'),
         pl.col('ALT').apply(nucleotide_to_index).alias('ALT_IDX')
     ])
     merged_df = pl.concat([df_positions, df_genotypes], how='horizontal')
 
+    # Store genotype data into HDF5
     with h5py.File(tmp_h5_file, 'a') as h5_gen_file:
         group_path = f'donor_{donor_id}/chr_{chromosome}'
         group = h5_gen_file.require_group(group_path)
@@ -74,12 +106,29 @@ def genotype_VCF2hdf5(data_path, donor_id, chromosome, save_path, study_name):
     logger.info(f"Finished processing VCF file for donor {donor_id} and chromosome {chromosome}")
 
 def process_chromosome(chromosome, vcf_dir, out_dir, study_name, donor_ids):
+    """
+    Process a single chromosome's VCF file and store the data into HDF5 files.
+
+    Parameters:
+    chromosome (int): Chromosome number to process.
+    vcf_dir (str): Directory containing VCF files.
+    out_dir (str): Directory to save the output HDF5 files.
+    study_name (str): Name of the study.
+    donor_ids (list): List of donor IDs to process.
+    """
     logger.info(f"Processing chromosome {chromosome} for donors {donor_ids}")
     vcf_file = os.path.join(vcf_dir, f'chr{chromosome}.filtered.vcf.gz')
     for donor_id in donor_ids:
         genotype_VCF2hdf5(vcf_file, donor_id, chromosome, out_dir, study_name)
 
 def merge_h5_files(tmp_dir, final_h5_file):
+    """
+    Merge temporary HDF5 files into a single final HDF5 file.
+
+    Parameters:
+    tmp_dir (str): Directory containing temporary HDF5 files.
+    final_h5_file (str): Path to the final HDF5 file.
+    """
     logger.info(f"Merging HDF5 files from {tmp_dir} to {final_h5_file}")
     with h5py.File(final_h5_file, 'a') as final_file:
         for tmp_file in os.listdir(tmp_dir):
