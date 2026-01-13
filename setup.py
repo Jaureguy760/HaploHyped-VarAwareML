7421b665998ee7b4f2a7ce18691e681e32f3fa97
Jeff Jaureguy <67065808+Jaureguy760@users.noreply.github.com>
2026-01-12 20:30:18 -0800

    fix: update author info and entry points

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='HaploHyped-VarAwareML',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='An end-to-end solution for processing genomic data and performing GPU-accelerated haplotype encoding for machine learning.',
+    author='Jeff Jaureguy',
+    author_email='jjaureguy@ucsd.edu',
+    description='High-performance VCF to HDF5 pipeline for genomic machine learning',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Jaureguy760/HaploHyped-VarAwareML',
@@ -23,13 +23,17 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'HaploHyped=haplohyped.main:main',
+            'vcf_to_h5=haplohyped.vcf_to_h5:main',
+            'fasta_encoder=haplohyped.fasta_encoder:main',
         ],
     },
     classifiers=[
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
     python_requires='>=3.8',
 )
